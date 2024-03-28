590cc750884264519949c14452f0695f43d8db6e
Seb Julliand <sjulliand@arcadsoftware.com>
2024-03-28 14:20:17 +0100

    Added runPRTRPGPRT command
    Signed-off-by: Seb Julliand <sjulliand@arcadsoftware.com>

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# VSCode Template extension
-This is my template to create a VScode extension that uses [Code for IBM i]([https://](https://github.com/codefori/vscode-ibmi))
+# Sample command extension for Code for i
+Created for ([this discussion](https://github.com/orgs/codefori/discussions/1963))
+
+## How to test
+1. Clone the repository
+2. Run `npm i`
+3. Open the project in VSCode
+4. Press `F5` to start the extension in debug mode
+
+## How to package
+1. Run npm `run package`
+2. Get the `.vsix`
--- src/commands.ts
+++ src/commands.ts
@@ -1,7 +1,42 @@
-import vscode from "vscode";
+import { MemberItem } from "@halcyontech/vscode-ibmi-types";
+import vscode, { l10n } from "vscode";
+import { code4i } from "./extension";
 
-export function initializeCommands(context: vscode.ExtensionContext){
-  context.subscriptions.push(
-		vscode.commands.registerCommand('sample.commandOnMember', (member: MemberItem) => vscode.window.showInformationMessage(vscode.l10n.t('Hello')))
+export function initializeCommands(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('sample.runPRTRPGPRT', async (memberItem: MemberItem) => {
+			//Run commands, print to output, etc
+			const connection = code4i.instance.getConnection();
+
+			//Run SQL, upload/download stuff
+			const content = code4i.instance.getContent();
+
+			//Prompt
+			const command = await vscode.window.showInputBox({
+				prompt: l10n.t("Prompt and run"),
+				value: `PRTRPGPRT SRCFILE(${memberItem.member.library}/${memberItem.member.file}) SRCMBR(${memberItem.member.name}) SUMMARY(*NO) OUTPUT(*PRINT) PRTFILE(qsysprt)`
+			});
+			
+			if (command) { //if prompt wasn't canceled
+				const result = await connection.runCommand({ command, environment: "ile" });
+				if(result.code === 0){
+					//success
+					console.log(result.stdout);
+					vscode.window.showInformationMessage(l10n.t("Command PRTRPGPRT successful"));
+
+					//get the spooled output data
+					const job = "the job"; //get the job info, maybe with SPOOLED_FILE_INFO ?
+					const [output] = await content.runSQL(`SELECT SPOOLED_DATA FROM TABLE(SYSTOOLS.SPOOLED_FILE_DATA(JOB_NAME => '${job}', SPOOLED_FILE_NAME =>'QSYSPRT'))`);
+					if(output.SPOOLED_DATA){
+						const data = String(output.SPOOLED_DATA);
+						//do something with the data
+					}
+				}
+				else{
+					//failure
+					vscode.window.showErrorMessage(l10n.t("Command PRTRPGPRT failed: {0}", result.stderr)); //show the error output in error notification
+				}
+			}
+		})
 	);
 }
